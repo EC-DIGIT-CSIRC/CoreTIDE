6de052d96dfcf824728ca66459f22ba1ec5e57ad
Amine BESSON <amine.besson@ext.ec.europa.eu>
2024-03-21 14:41:52 +0000

    feat: remove local navigation index header

--- Engines/templates/wiki_navigation.py
+++ Engines/templates/wiki_navigation.py
@@ -1,25 +1,8 @@
 NAV_INDEX_TEMPLATE = """
-
-## 🔗 Hotlinks
-
-- ❗ Create a New Issue : [TAM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=👺%20TAM&issuable_template=TAM%20Creation) | [TVM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=☣️%20TVM&issuable_template=TVM%20Creation) | [CDM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🛡️%20CDM&issuable_template=CDM%20Creation) | [BDR](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🏛%20BDR&issuable_template=BDR%20Creation) | [MDR](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🚨%20MDR&issuable_template=MDR%20Creation)
-- ❗ Create a Change Issue :  [TAM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=👺%20TAM%20CHANGE&issuable_template=TAM%20Change) | [TVM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=☣️%20TVM%20CHANGE&issuable_template=TVM%20Change) | [CDM](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🛡️%20CDM%20CHANGE&issuable_template=CDM%20Change) | [BDR](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🏛%20BDR%20CHANGE&issuable_template=BDR%20Change) | [MDR](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=🚨%20MDR%20CHANGE&issuable_template=MDR%20Change)
-- 🚨 [MDR Operations Board](https://git.s.cec.eu.int/catch/ec-tide/-/boards/23)
-- 🚧 [MDR Merge Requests](https://git.s.cec.eu.int/catch/ec-tide/-/merge_requests?label_name%5B%5D=MDR&scope=all&sort=created_date&state=opened)
-- 🚀 [Latest MDR Deployments](https://git.s.cec.eu.int/catch/ec-tide/-/environments)
-- 🚩 [Project Milestones](https://git.s.cec.eu.int/catch/ec-tide/-/milestones)
-- 🗿 [Model Development Board](https://git.s.cec.eu.int/catch/ec-tide/-/boards/22)
-- 💬 [EC-TIDE Ops Center](https://teams.microsoft.com/l/team/19%3a7LlWvSEdH28jqoX583V5O4vM_2oIQJz9ebotRcBKD4s1%40thread.tacv2/conversations?groupId=79716341-d825-44bb-a3cf-956a35a5d272&tenantId=b24c8b06-522c-46fe-9080-70926f8dddb1)
-- 🤔 [Workflow Procedures](https://git.s.cec.eu.int/catch/ec-tide/-/wikis/Procedures/%F0%9F%94%A5-MDR-Operations)
-- ⚠️ [Raise an incident](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issuable_template=incident&issue%5Bissue_type%5D=incident)
-- 💡[Suggest a new Feature](https://git.s.cec.eu.int/catch/ec-tide/-/issues/new?issue[title]=💡%20FEATURE&issuable_template=Feature%20Request)
-
-
 ## 🔎 Search EC-TIDE
 
 _Currently, Gitlab doesn't support links in those tables. You can use the right hand sidebar to navigate to the objects for the time being_
 
 {searches}
-
 
 """
